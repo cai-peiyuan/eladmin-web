370b3712e255f949451d9585093be7cfe93e96f4
peiyuan.cai-office <peiyuan.cai@mapabc.com>
2020-12-10 17:22:21 +0800

    代码生成工具修改

---
+++ src/views/msp/service/mapTokenApi/index.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" :hidden-columns="hiddenColumns" />
+      <!--表单组件-->
+      <el-dialog
+        :close-on-click-modal="false"
+        :before-close="crud.cancelCU"
+        :visible.sync="crud.status.cu > 0"
+        :title="crud.status.title"
+        width="500px"
+      >
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="100px">
+          <el-form-item label="序列号" prop="accessToken">
+            <el-input v-model="form.accessToken" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="服务地址" prop="apiUrl">
+            <el-input v-model="form.apiUrl" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="创建时间">
+            <el-input v-model="form.createTime" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="服务ID">
+            <el-input v-model="form.sid" :rows="3" type="textarea" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table
+        ref="table"
+        v-loading="crud.loading"
+        :data="crud.data"
+        stripe="stripe"
+        highlight-current-row="highlight-current-row"
+        size="small"
+        style="width: 100%;"
+        @selection-change="crud.selectionChangeHandler"
+        @sort-change="crud.changeSortHandler"
+      >
+        <el-table-column type="selection" width="55" fixed="left" />
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" inline class="demo-table-expand" style="padding-left: 60px">
+
+              <el-form-item label="序列号">
+                <span>{{ props.row.accessToken }}</span>
+              </el-form-item>
+              <el-form-item label="服务地址">
+                <span>{{ props.row.apiUrl }}</span>
+              </el-form-item>
+              <el-form-item label="创建时间">
+                <span>{{ parseTime(props.row.createTime) }}</span>
+              </el-form-item>
+              <el-form-item label="服务ID">
+                <span>{{ props.row.sid }}</span>
+              </el-form-item>
+
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column prop="accessToken" label="序列号" sortable="custom" />
+        <el-table-column prop="apiUrl" label="服务地址" sortable="custom" />
+        <el-table-column prop="createTime" label="创建时间" sortable="custom">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.createTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="sid" label="服务ID" sortable="custom" />
+        <el-table-column v-permission="['admin','mapTokenApi:edit','mapTokenApi:del']" label="操作" width="150px" align="center" fixed="right">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudMapTokenApi from '@/api/msp/mapTokenApi'
+import CRUD, { crud, form, header, presenter } from '@crud/crud'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = {
+  id: null,
+  accessToken: null,
+  apiUrl: null,
+  createTime: null,
+  createUser: null,
+  sid: null
+}
+export default {
+  name: 'MapTokenApi',
+  components: { pagination, crudOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  cruds() {
+    return CRUD({
+      title: '地图Token与接口权限关系',
+      url: 'api/mapTokenApi',
+      idField: 'id',
+      sort: 'id,desc',
+      crudMethod: { ...crudMapTokenApi }
+    })
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'mapTokenApi:add'],
+        edit: ['admin', 'mapTokenApi:edit'],
+        del: ['admin', 'mapTokenApi:del']
+      },
+      // 默认隐藏的数据列放到这个数组内 这里可以手动控制显示与隐藏 默认隐藏
+      hiddenColumns: [],
+      rules: {
+        accessToken: [
+          { required: true, message: '序列号不能为空', trigger: 'blur' }
+        ],
+        apiUrl: [
+          { required: true, message: '服务地址不能为空', trigger: 'blur' }
+        ]
+      }}
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .demo-table-expand {
+    font-size: 0;
+  }
+  .demo-table-expand label {
+    width: 70px;
+    color: #99a9bf;
+  }
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 100%;
+  }
+  .demo-table-expand .el-form-item__content {
+    font-size: 12px;
+  }
+  /deep/ .el-dialog__body {
+    padding: 0 20px 10px 20px !important;
+  }
+  .java.hljs {
+    color: #444444;
+    background: #ffffff !important;
+    height: 630px !important;
+  }
+  /deep/ .el-table__row {
+    cursor: pointer;
+  }
+</style>
