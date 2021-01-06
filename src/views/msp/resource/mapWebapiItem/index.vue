db83f1a49c7e0dad346e01ab455ca83317087c9e
peiyuan.cai-office <peiyuan.cai@mapabc.com>
2021-01-06 10:12:00 +0800

    Update index.vue

--- src/views/msp/resource/mapWebapiItem/index.vue
+++ src/views/msp/resource/mapWebapiItem/index.vue
@@ -6,6 +6,10 @@
         <!-- 搜索 -->
         <label class="el-form-item-label">资源名称</label>
         <el-input v-model="query.itemName" clearable placeholder="资源名称" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">资源类型</label>
+        <el-select v-model="query.itemType" clearable size="small" placeholder="资源类型" class="filter-item" style="width: 185px" @change="crud.toQuery">
+          <el-option v-for="item in dict.dict.MSP_RESOURCE_LIB_ITEM_TYPE" :key="item.value" :label="item.label" :value="item.value" />
+        </el-select>
         <rrOperation :crud="crud" />
       </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
