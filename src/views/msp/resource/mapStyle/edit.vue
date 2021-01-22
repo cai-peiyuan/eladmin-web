3ca82e68fc5b685bad619960d1482bd64808e9fa
peiyuan.cai-office <peiyuan.cai@mapabc.com>
2021-01-22 23:01:41 +0800

    Update edit.vue

--- src/views/msp/resource/mapStyle/edit.vue
+++ src/views/msp/resource/mapStyle/edit.vue
@@ -1,9 +1,29 @@
 <template>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="地图模板在线设计" />
-    <el-alert :closable="false" :title="'地图模板id：'+styleId" />
+  <div style="padding:0px;height: 100%">
+    <el-alert v-if="false" :closable="false" title="地图模板在线设计" />
+    <el-alert v-if="false" :closable="false" :title="'地图模板id：'+styleId" />
+    <iframe
+      src="http://localhost:8888/?layer=3448681199%7E0&modal=open#11.61/39.91/116.3938"
+      width="100%"
+      height="1400"
+      frameborder="no"
+      border="0"
+      marginwidth="0"
+      marginheight="0"
+      scrolling="no"
+      allowtransparency="yes"
+    />
   </div>
 </template>
+<style >
+  html,body
+  {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 100%;
+  }
+</style>
 <script >
 export default {
   data() {
